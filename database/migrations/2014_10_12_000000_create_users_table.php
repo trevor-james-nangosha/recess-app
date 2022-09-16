0ade71a171e7cb13d3614185d9af4559c3d50ecd
trevor-james-nangosha <trevornangosha16@gmail.com>
2022-09-16 18:57:29 +0300

    create users table

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -17,6 +17,9 @@ return new class extends Migration
             $table->id();
             $table->string('name');
             $table->string('email')->unique();
+            $table->string('type')->nullable();
+            $table->date('dateOfBirth')->nullable();
+            $table->string('shippingAddress')->nullable();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
