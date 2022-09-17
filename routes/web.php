8101727db1b7d12afb43562e1d8d109a55d3fe71
trevor-james-nangosha <trevornangosha16@gmail.com>
2022-09-17 10:20:28 +0300

    more routes

--- routes/web.php
+++ routes/web.php
@@ -16,23 +16,39 @@ use Illuminate\Support\Facades\Route;
 
 Auth::routes();
 
-//customer, user, participant
-
 Route::view('/', 'welcome');
 Route::view('/home', 'home');
 
-// Route::get('/login/customer', '');
-// Route::get('/login/admin', '');
-// Route::get('/login/participant', '');
+Route::get('/login/customer', 'App\Http\Controllers\Auth\LoginController@showCustomerLoginPage');
+Route::get('/login/admin', 'App\Http\Controllers\Auth\LoginController@showAdminLoginPage');
+Route::get('/login/participant', 'App\Http\Controllers\Auth\LoginController@showParticipantLoginPage');
+
+Route::get('/register/customer', 'App\Http\Controllers\Auth\RegisterController@showCustomerRegisterPage');
+Route::get('/register/admin', 'App\Http\Controllers\Auth\RegisterController@showAdminRegisterPage');
 
 // Route::get('/customer/{id}', '');
 // Route::get('/admin/{id}', '');
 // Route::get('/participant/{id}', '');
 
+// route();
+Route::post('/login/customer', 'App\Http\Controllers\Auth\LoginController@customerLogin')->name('loginCustomer');
+Route::post('/login/admin', 'App\Http\Controllers\Auth\LoginController@adminLogin')->name('loginAdmin');
+Route::post('/login/participant', 'App\Http\Controllers\Auth\LoginController@participantLogin')->name('loginParticipant');
 
-// Route::post('/login/customer', '');
-// Route::post('/login/admin', '');
-// Route::post('/login/participant', '');
+// TODO;
+// fix the issues below. especially from the route() method in my views.
+// this is bad naming practice.
+// fixed by using the name() method at the end.
+
+// when i try making post requests from my terminal using curl, all i get is the page expired page.
+// why is this happening????
+
+Route::post('/register/admin', 'App\Http\Controllers\Auth\RegisterController@createAdmin')->name('registerAdmin');
+Route::post('/register/customer', 'App\Http\Controllers\Auth\RegisterController@createCustomer')->name('registerCustomer');
+Route::post('/register/participant', 'App\Http\Controllers\Auth\RegisterController@createParticipant')->name('registerParticipant');
+
+
+
 
 
 
