ddda9c71fa0ac2b8c413cc2bcafcb6425084cde1
trevor-james-nangosha <trevornangosha16@gmail.com>
2022-09-17 10:21:58 +0300

    made email field nullable

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -16,7 +16,7 @@ return new class extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->string('email')->unique();
+            $table->string('email')->unique()->nullable();
             $table->string('type')->nullable();
             $table->date('dateOfBirth')->nullable();
             $table->string('shippingAddress')->nullable();
