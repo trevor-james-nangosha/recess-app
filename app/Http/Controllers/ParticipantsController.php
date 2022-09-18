a80db2d2dc2f3430bda05ddc3995c29b7405c808
trevor-james-nangosha <trevornangosha16@gmail.com>
2022-09-18 23:28:42 +0300

    new participant controller
    serve requests that require browsing all participants

---
+++ app/Http/Controllers/ParticipantsController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ParticipantsController extends Controller
+{
+    public function browseAllParticipants(){
+        // code...
+    }
+
+    public function browseParticipant(Request $request){
+        // code...
+    }
+}
