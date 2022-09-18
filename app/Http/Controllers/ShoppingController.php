e39b22c37eec21ca93d04522bee66a294137f007
trevor-james-nangosha <trevornangosha16@gmail.com>
2022-09-18 09:40:21 +0300

    new controllers

---
+++ app/Http/Controllers/ShoppingController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ShoppingController extends Controller
+{
+    public function showOrderPage(){
+        // code...
+    }
+}
