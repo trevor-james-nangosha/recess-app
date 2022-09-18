84412609e885e300811ff1f13f6eab2aa9fd91e4
trevor-james-nangosha <trevornangosha16@gmail.com>
2022-09-18 23:30:06 +0300

    products controller
    when a customer wants to see the list of all products.

--- app/Http/Controllers/ProductsController.php
+++ app/Http/Controllers/ProductsController.php
@@ -3,10 +3,19 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Laravel\Ui\Presets\React;
 
 class ProductsController extends Controller
 {
-    public function showProducts(){
+    public function showAllProducts(){
+        // code...
+    }
+
+    public function showProduct(Request $request){
+        // code...
+    }
+
+    public function addNewProduct(Request $request){
         // code...
     }
 }
