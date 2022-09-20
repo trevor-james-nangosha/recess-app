ffd52169e1e7a16492ae837dba436cbe000d4310
trevor-james-nangosha <trevornangosha16@gmail.com>
2022-09-21 02:07:12 +0300

    insignificant

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,8 @@ export default defineConfig({
     plugins: [
         laravel({
             input: ['resources/css/app.css', 'resources/js/app.js'],
-            refresh: true,
+            refresh: false, // set this to false to prevent reloads every time app.js/app.css changes. you have to do it manually.
+            // but i maybe wrong.
         }),
     ],
 });
