8e78a02ba5e3c7a1b90f1bbd24603e1fdbce9109
trevor-james-nangosha <trevornangosha16@gmail.com>
2022-09-21 02:21:23 +0300

    front end s**t

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -12,6 +12,7 @@
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
     <link href="https://fonts.bunny.net/css?family=Nunito" rel="stylesheet">
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 
     <!-- Scripts -->
     @vite(['resources/sass/app.scss', 'resources/js/app.js'])
@@ -76,5 +77,7 @@
             @yield('content')
         </main>
     </div>
+    @stack('scripts')
 </body>
 </html>
+
